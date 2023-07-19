81261c403ca36c68cab8f46cf206e3c6f2b18587
dranyam2022 <maynardescalante@gmail.com>
2023-07-19 14:45:15 +0800

    completed logic for show.ejs

--- app.js
+++ app.js
@@ -21,10 +21,13 @@ app.get("/", (req, res) => {
   res.render("home");
 });
 
-app.get("/makecampground", async (req, res) => {
-  const camp = new Campground({ title: "My Backyard", description: 'cheap camping!'});
-  await camp.save()
-  res.send(camp)
+app.get("/campgrounds", async (req, res) => {
+  const campgrounds = await Campground.find({});
+  res.render("campgrounds/index", { campgrounds });
+});
+
+app.get("/campgrounds/:id", async (req, res) => {
+  res.render("campgrounds/show", {});
 });
 
 app.listen(port, () => {

d1a14af36552712e759bdd43803896bfeab55872
dranyam2022 <maynardescalante@gmail.com>
2023-07-25 08:40:33 +0800

    completed logic for campgrounds/new route

--- app.js
+++ app.js
@@ -17,6 +17,9 @@ db.once("open", () => {
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.render("home");
 });
@@ -26,8 +29,27 @@ app.get("/campgrounds", async (req, res) => {
   res.render("campgrounds/index", { campgrounds });
 });
 
+app.get("/campgrounds/new", (req, res) => {
+  res.render("campgrounds/new");
+});
+
+app.post("/campgrounds", async (req, res) => {
+  const campground = new Campground(req.body.campground);
+  await campground.save();
+  res.redirect(`/campgrounds/${campground._id}`);
+});
+
 app.get("/campgrounds/:id", async (req, res) => {
-  res.render("campgrounds/show", {});
+  try {
+    const campground = await Campground.findById(req.params.id);
+    if (!campground) {
+      return res.status(404).send("Campground not found");
+    }
+    res.render("campgrounds/show", { campground });
+  } catch (error) {
+    console.error(err);
+    res.status(500).send("Internal Server Error");
+  }
 });
 
 app.listen(port, () => {

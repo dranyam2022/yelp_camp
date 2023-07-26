374ec6b8bece880c5392bab0fc21087b5d789e0f
dranyam2022 <maynardescalante@gmail.com>
2023-07-26 11:42:08 +0800

    completed basic styling

--- seeds/index.js
+++ seeds/index.js
@@ -15,11 +15,16 @@ const sample = (array) => array[Math.floor(Math.random() * array.length)];
 
 const seedDB = async () => {
   await Campground.deleteMany({});
-  for (let i = 0; i < 50; i++) {
+  for (let i = 0; i < 10; i++) {
     const random1000 = Math.floor(Math.random() * 1000);
+    const price = Math.floor(Math.random() * 30);
     const camp = new Campground({
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
       title: `${sample(descriptors)} ${sample(places)}`,
+      image: "https://source.unsplash.com/collection/483251",
+      description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam at voluptatum eveniet doloribus, voluptatem exercitationem ullam quae, officiis perspiciatis fuga, enim sequi! Excepturi dicta doloremque inventore alias deleniti, sint enim.
+      Veritatis similique rerum minima assumenda mollitia repudiandae quos saepe laudantium, itaque voluptatum corrupti iure consequuntur veniam doloribus alias soluta fugit accusantium eveniet adipisci, at dicta recusandae, nisi reiciendis! Aut, sequi?`,
+      price: `$${price}.00`,
     });
     await camp.save();
   }
